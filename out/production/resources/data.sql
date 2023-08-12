bf72a77f603e8d642ed5f3d1d435a5a267469a69
beompp <71520013+beompp@users.noreply.github.com>
2023-08-12 20:16:43 +0900

    11장 HTTP와 REST 컨트롤러
    11.3.1 REST 컨트롤러 맛보기

---
+++ out/production/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO article(title, content) VALUES ('가가가가', '1111');
+INSERT INTO article(title, content) VALUES ('나나나나', '2222');
+INSERT INTO article(title, content) VALUES ('다다다다', '3333');
+
+INSERT INTO member(email, password) VALUES ('11@11.com', '1111');
+INSERT INTO member(email, password) VALUES ('22@22.com', '2222');
+INSERT INTO member(email, password) VALUES ('33@33.com', '3333');
--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -60,6 +61,59 @@ public class MemberController {
 
         // 3. 뷰페이지로 모델 전달
         return "members/index";
+    }
+
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // 1. id값 불러와서 entity에 담기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+
+        // 2. entity를 model에 담기
+        model.addAttribute("member", memberEntity);
+
+        // 3. 뷰페이지로 모델 전달
+        return "/members/edit";
+    }
+
+    @PostMapping("/members/update")
+    public String update(MemberForm form) {
+        // 1. DTO를 엔티티로 변환
+        Member memberEntity = form.toEntity();
+
+        // 2. 리파지토리로 엔티티를 DB에 저장
+        // 2-1. id가 db에 있는 데이터인지 확인하고 저장
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+
+        if (target != null) {
+            memberRepository.save(memberEntity);
+        }
+        /*
+        if (memberEntity.getId() != null) {
+            memberRepository.save(memberEntity);
+        }
+        */
+
+        // 3. 상세페이지(뷰)로 리다이렉트
+        return "redirect:/members/" + memberEntity.getId();
+    }
+
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제요청");
+
+        // 1. 삭제할 id 조회
+        Member target = memberRepository.findById(id).orElse(null);
+        log.info("삭제할 id 조회 ::::: " + target.toString());
+
+        // 2. 해당 id가 있으면 메시지 띄우고 계정 삭제
+        if (target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제 완료");
+            log.info("삭제 완료 ::::: " + target.toString());
+        }
+
+        // 3. 목록 화면으로 이동
+        return "/members";
     }
 
 }
